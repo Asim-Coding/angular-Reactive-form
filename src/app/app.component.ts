535ebe168f84957379b593a6a215857237581903
Asim-Coding <muhammadasim19955@gmail.com>
2022-05-17 13:24:06 +0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms'
+import {FormControl,FormGroup,Validators} from '@angular/forms'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -8,11 +8,18 @@ import {FormControl,FormGroup} from '@angular/forms'
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   loginForm = new FormGroup({
-    user: new FormControl(''),
-    password: new FormControl('')
+    user: new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+    password: new FormControl('',[Validators.required,Validators.minLength(3)])
   })
 
   loginUser(){
     console.warn(this.loginForm.value);
   }
+  get user(){
+    return this.loginForm.get('user');
+  }
+  get password(){
+    return this.loginForm.get('password');
+  }
 }
+
